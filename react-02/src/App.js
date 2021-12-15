542eaa0a339c86bcdf1b0de26f22bd9ce9c1cde2
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-12-15 21:07:56 +0530

    delete option added

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -1,14 +1,17 @@
+import Todo from './components/Todo';
+import Modal from './components/Modal';
+import Backdrop from './components/Backdrop';
+
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <div className='card'>
-        <h2>TITLE</h2>
-        <div className='actions'>
-          <button className='btn'>Delete</button>
-        </div>
-      </div>
-    </div> //JSX code
+      <Todo text='Begin React' />
+      <Todo text='Do some projects' />
+      <Todo text='Finish-up React' />
+      <Modal />
+      <Backdrop />
+    </div>
   );
 }
 export default App;
