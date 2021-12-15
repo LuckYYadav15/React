d36dfcc5ace93275a0b6b115e3708f1a6daa8a7f
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-12-15 17:07:17 +0530

    inital steps for project-2

---
+++ react-02/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <div className='card'>
+        <h2>TITLE</h2>
+        <div className='actions'>
+          <button className='btn'>Delete</button>
+        </div>
+      </div>
+    </div> //JSX code
+  );
+}
+export default App;
---
+++ react-02/src/index.js
@@ -0,0 +1,14 @@
+// starting point of our project
+// HTMl in js is known as jSX
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
