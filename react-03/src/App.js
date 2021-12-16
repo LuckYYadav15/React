ea3fea4e3f238790f3e7fc8b8325498f0f334e92
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-12-16 20:13:49 +0530

    navigation with active links added

--- react-03/src/App.js
+++ react-03/src/App.js
@@ -2,14 +2,16 @@ import { Route, Switch } from 'react-router-dom';
 
 import AllMeetupsPage from './components/pages/AllMeetups';
 import NewMeetupPage from './components/pages/NewMeetup';
-import FavouritesPage from './components/pages/Favourites';
+import FavoritesPage from './components/pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
-        <Route path='/'>
+        <Route path='/' exact>
           <AllMeetupsPage />
         </Route>
 
@@ -17,15 +19,14 @@ function App() {
           <NewMeetupPage />
         </Route>
 
-        <Route path='/favourites'>
-          <FavouritesPage />
+        <Route path='/favorites'>
+          <FavoritesPage />
         </Route>
       </Switch>
     </div>
-
   );
 }
 
 export default App;
 
-//npm install react-router-dom@5.3.0
+//npm install react-router-dom@5.2.0
