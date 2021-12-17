f893b80db72be319ca316f722107856e215e3ee7
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-12-17 10:26:09 +0530

    modified listed data

--- react-03/src/components/pages/AllMeetups.js
+++ react-03/src/components/pages/AllMeetups.js
@@ -1,15 +1,17 @@
+import MeetupList from "../meetups/MeetupList";
+
 const DUMMY_DATA = [
     {
         id: 'm1',
         title: 'This is a first meetup',
-        image: 'https://en.wikipedia.org/wiki/Munich#/media/File:Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG',
+        image: '../images/street_1.jpeg',
         address: 'Meetupstreet 5, 12345 Meetup City',
         description: 'This is a first, amazing mmetup which you definitely should not miss. It will be great if you read it.'
     },
     {
         id: 'm2',
         title: 'This is second meetup',
-        image: 'https://en.wikipedia.org/wiki/Munich#/media/File:Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG',
+        image: '../images/street_2.jpeg',
         address: 'Meetupstreet 12, 67890 Meetup City',
         description: 'This is a second, amazing mmetup which you definitely should not miss. It will be great if you read it.'
     }
@@ -19,11 +21,7 @@ function AllMeetupsPage() {
     return (
         <section>
             <h1>All Meetups</h1>
-            <ul>
-                {DUMMY_DATA.map((meetup) => {
-                    return <li key={meetup.id}>{meetup.title}</li>
-                })}
-            </ul>
+            <MeetupList meetups={DUMMY_DATA} />
         </section>
     );
 }
