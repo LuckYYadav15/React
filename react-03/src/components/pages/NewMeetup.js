8b0c0e6a334226b750b07912e7805b5069dd1048
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-12-18 11:07:50 +0530

    sending POST request to Firebase

--- react-03/src/components/pages/NewMeetup.js
+++ react-03/src/components/pages/NewMeetup.js
@@ -1,9 +1,19 @@
 import NewMeetupForm from "../meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+    function addMeetupHandler(meetupData) {
+        fetch('https://react-getting-started-7d7a3-default-rtdb.firebaseio.com/meetups.json', {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+
     return <section>
         <h1>Add New Meetup</h1>
-        <NewMeetupForm/>
+        <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
 }
 
