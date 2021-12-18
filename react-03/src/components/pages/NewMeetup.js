587944c6efa0ac3865648197499e6aebf2df8528
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-12-18 10:21:44 +0530

    form added

--- react-03/src/components/pages/NewMeetup.js
+++ react-03/src/components/pages/NewMeetup.js
@@ -1,6 +1,9 @@
+import NewMeetupForm from "../meetups/NewMeetupForm";
+
 function NewMeetupPage() {
     return <section>
         <h1>Add New Meetup</h1>
+        <NewMeetupForm/>
     </section>
 }
 
