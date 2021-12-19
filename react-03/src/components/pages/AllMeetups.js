70657a5a5f53ed34f8483efe0f8944a45be99e5e
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-12-19 22:38:02 +0530

    comments removed

--- react-03/src/components/pages/AllMeetups.js
+++ react-03/src/components/pages/AllMeetups.js
@@ -1,24 +1,5 @@
-// import { response } from "express";
-// import res from "express/lib/response";
 import { useState, useEffect } from "react";
 import MeetupList from "../meetups/MeetupList";
-
-// const DUMMY_DATA = [
-//     {
-//         id: 'm1',
-//         title: 'This is a first meetup',
-//         image: '../images/street_1.jpeg',
-//         address: 'Meetupstreet 5, 12345 Meetup City',
-//         description: 'This is a first, amazing mmetup which you definitely should not miss. It will be great if you read it.'
-//     },
-//     {
-//         id: 'm2',
-//         title: 'This is second meetup',
-//         image: '../images/street_2.jpeg',
-//         address: 'Meetupstreet 12, 67890 Meetup City',
-//         description: 'This is a second, amazing mmetup which you definitely should not miss. It will be great if you read it.'
-//     }
-// ]
 
 function AllMeetupsPage() {
     const [isLoading, setIsLoading] = useState(true);
