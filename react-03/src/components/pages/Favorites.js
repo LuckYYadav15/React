7858ae90c825d16a5058a63f2d9fd1c6936aa29b
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-12-21 00:28:25 +0530

    more context usage

--- react-03/src/components/pages/Favorites.js
+++ react-03/src/components/pages/Favorites.js
@@ -1,5 +1,23 @@
+import { useContext } from 'react';
+
+import FavouritesContext from '../../store/favorites-context';
+import MeetupList from '../meetups/MeetupList';
+
 function FavoritesPage() {
-    return <div>Favorites Page</div>
+    const favoritesCtx = useContext(FavouritesContext);
+
+    let content;
+
+    if (favoritesCtx.totalfavorites === 0) {
+        content = <p>You got no favorites yet. Start adding some?</p>
+    }
+    else {
+        content = <MeetupList meetups={favoritesCtx.favorites} />
+    }
+    return (<section>
+        <h1>My Favorites</h1> 
+        {content}
+    </section>);
 }
 
 export default FavoritesPage;
